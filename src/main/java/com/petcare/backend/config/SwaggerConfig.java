70d7e7e14b7ce03ebdcd48dcb5081642574add58
Lehaibac05 <bac.le@software.codegym.vn>
2026-08-20 23:21:55 +0700

    fix error for mobile

--- src/main/java/com/petcare/backend/config/SwaggerConfig.java
+++ src/main/java/com/petcare/backend/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,7 +13,9 @@ import org.springframework.context.annotation.Configuration;
     title = "CForum APIs Document",
     version = "6.6.6", 
     description = "API Documentation"), 
-    security = {@SecurityRequirement(name = "bearerAuth") })
+    security = {@SecurityRequirement(name = "bearerAuth") },
+    servers = {@Server(url = "https://api.petdiarycare.io.vn", description = "Local server") }
+    )
 @SecurityScheme(name = "bearerAuth", 
     type = SecuritySchemeType.HTTP, 
     scheme = "bearer", 
